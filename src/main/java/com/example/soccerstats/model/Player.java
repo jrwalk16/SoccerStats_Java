06e28972aaada2c04a568a8613a934964a9e1452
jeremy <jeremywalker1216@gmail.com>
2022-02-27 09:00:34 -0500

    added packages began player class

---
+++ src/main/java/com/example/soccerstats/model/Player.java
@@ -0,0 +1,17 @@
+package com.example.soccerstats.model;
+
+
+
+
+public class Player {
+
+    private Long id;
+
+
+    private String playerName;
+
+    private String position;
+
+    private
+
+}
