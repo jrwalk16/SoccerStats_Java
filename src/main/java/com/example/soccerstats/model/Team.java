71d518a38a8864560333b394c53638c44e75807f
jeremy <jeremywalker1216@gmail.com>
2022-02-27 11:45:31 -0500

    finished team model

--- src/main/java/com/example/soccerstats/model/Team.java
+++ src/main/java/com/example/soccerstats/model/Team.java
@@ -1,4 +1,68 @@
 package com.example.soccerstats.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "teams")
 public class Team {
+
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column
+    private String teamName;
+
+
+    @Column
+    private String teamCity;
+
+    @Column
+    private String teamCountry;
+
+    public Team() {
+    }
+
+    public Team(Long id, String teamName, String teamCity, String teamCountry) {
+        this.id = id;
+        this.teamName = teamName;
+        this.teamCity = teamCity;
+        this.teamCountry = teamCountry;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getTeamCity() {
+        return teamCity;
+    }
+
+    public void setTeamCity(String teamCity) {
+        this.teamCity = teamCity;
+    }
+
+    public String getTeamCountry() {
+        return teamCountry;
+    }
+
+    public void setTeamCountry(String teamCountry) {
+        this.teamCountry = teamCountry;
+    }
 }
