6e2626d56dfa97650d3a129ea6c27d9b3d3cf084
jeremy <jeremywalker1216@gmail.com>
2022-02-27 08:42:27 -0500

    first commit

---
+++ src/main/java/com/example/soccerstats/SoccerstatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.soccerstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerstatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerstatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/soccerstats/SoccerstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soccerstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerstatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
